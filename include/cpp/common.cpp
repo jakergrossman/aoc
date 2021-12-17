7ed4229b43041a36125917bcc950dddd91d1fad1
jakergrossman <jake.r.grossman@gmail.com>
2021-12-16 22:36:50 -0600

    style(cpp): fix whitespace mistakes

--- include/cpp/common.cpp
+++ include/cpp/common.cpp
@@ -15,44 +15,44 @@ high_resolution_clock::time_point AOC::Timer::stop_;
 
 high_resolution_clock::time_point AOC::Timer::start()
 {
-	AOC::Timer::start_ = time_point_cast<nanoseconds>(high_resolution_clock::now());
-	return AOC::Timer::start_;
+    AOC::Timer::start_ = time_point_cast<nanoseconds>(high_resolution_clock::now());
+    return AOC::Timer::start_;
 }
 
 high_resolution_clock::time_point AOC::Timer::stop()
 {
-	AOC::Timer::stop_ = time_point_cast<nanoseconds>(high_resolution_clock::now());
-	return stop_;
+    AOC::Timer::stop_ = time_point_cast<nanoseconds>(high_resolution_clock::now());
+    return stop_;
 }
 
 duration<double> AOC::Timer::elapsed()
 {
-	duration<double> elapsed_ = duration_cast<duration<double>>(AOC::Timer::stop_ - Timer::start_);
-	return elapsed_;
+    duration<double> elapsed_ = duration_cast<duration<double>>(AOC::Timer::stop_ - Timer::start_);
+    return elapsed_;
 }
 
 std::string AOC::Timer::human_readable(duration<double> duration) {
-	auto t = duration.count();
-	std::string unit;
-	unsigned int magnitude;
+    auto t = duration.count();
+    std::string unit;
+    unsigned int magnitude;
 
-	if (t >= 1) {
-		unit = "s";
-		magnitude = t;
-	}
-	else if (t >= 0.001) {
-		unit = "ms";
-		magnitude = t * 1000;
-	}
-	else if (t >= 0.000001) {
-		unit = "μs";
-		magnitude = t * 1000000;
-	} else {
-		unit = "ns";
-		magnitude = t * 1000000000;
-	}
+    if (t >= 1) {
+        unit = "s";
+        magnitude = t;
+    }
+    else if (t >= 0.001) {
+        unit = "ms";
+        magnitude = t * 1000;
+    }
+    else if (t >= 0.000001) {
+        unit = "μs";
+        magnitude = t * 1000000;
+    } else {
+        unit = "ns";
+        magnitude = t * 1000000000;
+    }
 
-	return std::to_string((unsigned int)std::floor(magnitude)) + " " + unit;
+    return std::to_string((unsigned int)std::floor(magnitude)) + " " + unit;
 }
 
 template void AOC::output_result<unsigned int>(unsigned int part_one, unsigned int part_two, duration<double> elapsed);
@@ -69,17 +69,17 @@ void AOC::output_result(T part_one, T part_two, duration<double> elapsed)
 
 // read lines of input
 std::vector<std::string> AOC::get_lines(const char* filename) {
-	std::ifstream input_file(filename);
-	if (!input_file) {
-		std::cerr << "Could not open " << filename << std::endl;
-		std::exit(1);
-	}
-
-	std::vector<std::string> lines;
-    std::string line;
-    while (std::getline(input_file, line)) {
-		lines.push_back(line);
+    std::ifstream input_file(filename);
+    if (!input_file) {
+        std::cerr << "Could not open " << filename << std::endl;
+        std::exit(1);
     }
 
-	return lines;
+    std::vector<std::string> lines;
+    std::string line;
+    while (std::getline(input_file, line)) {
+        lines.push_back(line);
+    }
+
+    return lines;
 }
--- include/cpp/common.h
+++ include/cpp/common.h
@@ -7,17 +7,17 @@
 using namespace std::chrono;
 
 namespace AOC {
-	namespace Timer {
-		extern high_resolution_clock::time_point start_;
-		extern high_resolution_clock::time_point stop_;
-		high_resolution_clock::time_point start();
-		high_resolution_clock::time_point stop();
-		duration<double> elapsed();
-		std::string human_readable(duration<double>);
-	}
+    namespace Timer {
+        extern high_resolution_clock::time_point start_;
+        extern high_resolution_clock::time_point stop_;
+        high_resolution_clock::time_point start();
+        high_resolution_clock::time_point stop();
+        duration<double> elapsed();
+        std::string human_readable(duration<double>);
+    }
 
-	template <typename T>
-	void output_result(T part_one, T part_two, duration<double> elapsed);
+    template <typename T>
+    void output_result(T part_one, T part_two, duration<double> elapsed);
 
-	std::vector<std::string> get_lines(const char* filename);
+    std::vector<std::string> get_lines(const char* filename);
 }
